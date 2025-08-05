5b12aed6d3aaece6403f2b710479c118a3443b64
manasi-navale2107 <navalemanasi@gmail.com>
2025-08-06 00:07:51 +0530

    Time: 0 ms (100.00%) | Memory: 41.8 MB (28.95%) - LeetSync

---
+++ 1970-sorting-the-sentence/sorting-the-sentence.java
@@ -0,0 +1,25 @@
+class Solution {
+    public String sortSentence(String s) {
+
+        String[] str = s.split(" ");
+        String[] arr = new String[str.length];
+
+        for(String t:str){
+            int n=t.length();
+
+            int pos=t.charAt(n-1)-'0';
+            arr[pos-1]=t.substring(0,n-1);
+            
+        }
+
+        StringBuilder sb = new StringBuilder();
+
+        for(String t:arr){
+            sb.append(t);
+            sb.append(" ");
+        }
+       
+        return sb.toString().trim();
+        
+    }
+}
