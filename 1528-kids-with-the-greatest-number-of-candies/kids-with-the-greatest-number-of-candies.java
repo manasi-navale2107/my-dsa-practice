d86af9e027f651077acc67ee694d039f0ee0cb3b
manasi-navale2107 <navalemanasi@gmail.com>
2025-08-07 13:45:56 +0530

    Time: 0 ms (100.00%) | Memory: 42.4 MB (56.49%) - LeetSync

---
+++ 1528-kids-with-the-greatest-number-of-candies/kids-with-the-greatest-number-of-candies.java
@@ -0,0 +1,23 @@
+class Solution {
+    public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+        int max = Integer.MIN_VALUE;
+
+        for(int i : candies){
+            if(i>max){
+                max=i;
+            }
+        }
+
+        List<Boolean> res = new ArrayList<>();
+
+        for(int i: candies){
+            if(i+extraCandies>=max){
+                res.add(true);
+            }
+            else{
+                res.add(false);
+            }
+        }
+        return res;
+    }
+}
