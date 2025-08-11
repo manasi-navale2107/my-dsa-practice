fecc9befa6dfa77bf91823ccd8d313937d357aa0
manasi-navale2107 <navalemanasi@gmail.com>
2025-08-11 16:54:27 +0530

    Time: 0 ms (100.00%) | Memory: 41.6 MB (64.72%) - LeetSync

--- 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.java
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.java
@@ -4,3 +4,24 @@ class Solution {
         
     }
 }
+
+/*class Solution {
+    public int strStr(String haystack, String needle) {
+        int n = haystack.length();
+        int m = needle.length();
+
+        if (m == 0) return 0; // empty needle → index 0
+
+        for (int i = 0; i <= n - m; i++) {
+            int j;
+            for (j = 0; j < m; j++) {
+                if (haystack.charAt(i + j) != needle.charAt(j)) {
+                    break; // mismatch, move to next position
+                }
+            }
+            if (j == m) return i; // found full match
+        }
+        return -1; // not found
+    }
+}
+*/
