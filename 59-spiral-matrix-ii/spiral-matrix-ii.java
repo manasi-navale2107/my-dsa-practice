a50beb42a7192dae4e7a378003879b1dbdc46793
manasi-navale2107 <navalemanasi@gmail.com>
2025-08-16 23:28:07 +0530

    Time: 0 ms (100.00%) | Memory: 41.8 MB (17.20%) - LeetSync

---
+++ 59-spiral-matrix-ii/spiral-matrix-ii.java
@@ -0,0 +1,41 @@
+class Solution {
+    public int[][] generateMatrix(int n) {
+        int[][] result=new int[n][n];
+        int top=0; 
+        int left=0;
+        int bottom=n-1; 
+        int right=n-1;
+        int num =1;
+        while(top<=bottom && left<=right){
+
+            for(int i=top; i<=right; i++){
+                result[top][i]=num++;
+            }
+            top++;
+
+            for(int i=top; i<=bottom; i++){
+                result[i][right]=num++;
+            }
+            right--;
+
+            if(top<=bottom){
+                for(int i=right; i>=left; i--){
+                    result[bottom][i]=num++;
+                }
+                 bottom--;
+
+            }    
+               
+            if(left<=right){
+                for(int i=bottom; i>=top; i--){
+                    result[i][left]=num++;
+                }
+                
+                left++;
+            }    
+            
+        }
+        return result;
+        
+    }
+}
