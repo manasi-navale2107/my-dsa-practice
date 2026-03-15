b8353e00b9f040fbb9b0475d171045c727c4bad6
manasi-navale2107 <navalemanasi@gmail.com>
2026-03-15 18:23:24 +0530

    Time: 0 ms (100.00%) | Memory: 46.7 MB (34.25%) - LeetSync

---
+++ 1185-find-in-mountain-array/find-in-mountain-array.java
@@ -0,0 +1,69 @@
+class Solution {
+
+    public int findInMountainArray(int target, MountainArray mountainArr) {
+
+        int peak = peakIndexInMountainArray(mountainArr);
+
+        int firstTry = orderOftheArray(mountainArr, target, 0, peak);
+
+        if(firstTry != -1){
+            return firstTry;
+        }
+
+        return orderOftheArray(mountainArr, target, peak + 1, mountainArr.length() - 1);
+    }
+
+
+    public int peakIndexInMountainArray(MountainArray arr){
+        int start = 0;
+        int end = arr.length() - 1;
+
+        while(start < end){
+
+            int mid = start + (end - start) / 2;
+
+            if(arr.get(mid) > arr.get(mid + 1)){
+                end = mid;
+            }else{
+                start = mid + 1;
+            }
+        }
+
+        return start;
+    }
+
+
+    public int orderOftheArray(MountainArray arr, int target, int start, int end){
+
+        boolean isAsc = arr.get(start) < arr.get(end);
+
+        while(start <= end){
+
+            int mid = start + (end - start) / 2;
+
+            if(arr.get(mid) == target){
+                return mid;
+            }
+
+            if(isAsc){
+
+                if(target > arr.get(mid)){
+                    start = mid + 1;
+                }else{
+                    end = mid - 1;
+                }
+
+            }else{
+
+                if(target > arr.get(mid)){
+                    end = mid - 1;
+                }else{
+                    start = mid + 1;
+                }
+
+            }
+        }
+
+        return -1;
+    }
+}
