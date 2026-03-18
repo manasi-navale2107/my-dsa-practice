4013b7923c24e9b060e704381888bedd511624cb
manasi-navale2107 <navalemanasi@gmail.com>
2026-03-18 17:33:02 +0530

    Time: 0 ms (100.00%) | Memory: 42.8 MB (75.69%) - LeetSync

--- 1205-defanging-an-ip-address/defanging-an-ip-address.java
+++ 1205-defanging-an-ip-address/defanging-an-ip-address.java
@@ -1,23 +1,5 @@
 class Solution {
     public String defangIPaddr(String address) {
-
-        //return address.replace(".", "[.]");
-
-        StringBuilder defanged = new StringBuilder();
-        for (int i = 0; i < address.length(); i++) {
-            char ch = address.charAt(i);
-            if (ch == '.') {
-                defanged.append("[.]");  
-            } else {
-                defanged.append(ch);     
-            }
-        }
-
-        return defanged.toString();
+         return address.replace(".","[.]");
     }
 }
-
-
-
-        
- 
