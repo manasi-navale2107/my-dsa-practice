629cce4251f3097e8e2fb1f122f30ee6c48374b2
manasi-navale2107 <navalemanasi@gmail.com>
2026-04-16 16:17:37 +0530

    Time: 2 ms (39.55%) | Memory: 94.5 MB (56.24%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.java
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.java
@@ -6,9 +6,9 @@ class Solution {
         for(int i=1; i<prices.length; i++){
             if(prices[i] < min){
                 min = prices[i];
-            }else{
-                profit = Math.max(profit, prices[i] - min);
             }
+
+            profit = Math.max(profit, prices[i] - min);
         }
 
         return profit;
