ef7de8d18dcf3a9367582e727059afa515917a64
manasi-navale2107 <navalemanasi@gmail.com>
2026-05-18 18:51:09 +0530

    Time: 2 ms (53.26%) | Memory: 43.9 MB (17.90%) - LeetSync

--- 10-regular-expression-matching/regular-expression-matching.java
+++ 10-regular-expression-matching/regular-expression-matching.java
@@ -39,7 +39,7 @@ class Solution {
             }
         }
             
-            return dp[s.length()][p.length()];
+            return  dp[s.length()][p.length()];
             
             
             
