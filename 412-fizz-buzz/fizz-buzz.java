a1be085d1051c4a15cb1dda95dddf57a463e1201
manasi-navale2107 <navalemanasi@gmail.com>
2026-06-01 09:47:49 +0530

    Time: 1 ms (99.92%) | Memory: 46.9 MB (45.83%) - LeetSync

--- 412-fizz-buzz/fizz-buzz.java
+++ 412-fizz-buzz/fizz-buzz.java
@@ -1,17 +1,20 @@
-public class Solution {
+class Solution {
     public List<String> fizzBuzz(int n) {
+        
         List<String> result = new ArrayList<>();
-        for (int i = 1; i <= n; i++) {
-            if (i % 3 == 0 && i % 5 == 0) {
+
+        for(int i=1; i<=n; i++){
+            if( i%3 == 0 && i%5==0){
                 result.add("FizzBuzz");
-            } else if (i % 3 == 0) {
+            }else if( i%3 == 0){
                 result.add("Fizz");
-            } else if (i % 5 == 0) {
+            }else if(i%5 == 0){
                 result.add("Buzz");
-            } else {
+            }else{
                 result.add(Integer.toString(i));
             }
         }
+
         return result;
     }
 }
