36759ef96bd2df59fb0bd6eed6e4ea4c9d8c43ec
manasi-navale2107 <navalemanasi@gmail.com>
2026-06-02 16:22:39 +0530

    Time: 13 ms (41.66%) | Memory: 12.3 MB (89.33%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def isPalindrome(self, x):
+        """
+        :type x: int
+        :rtype: bool
+        """
+
+        if x < 0:
+            return False
+        num = x
+        result = 0;
+
+        while num > 0:
+          ld = num % 10
+          result = result * 10 + ld
+          num = num//10
+        
+        return x == result
+        
