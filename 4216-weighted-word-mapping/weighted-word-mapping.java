27fbf586516b2d82d01b49e1940421a7bb070d29
manasi-navale2107 <navalemanasi@gmail.com>
2026-06-13 23:22:55 +0530

    Time: 2 ms (96.85%) | Memory: 46.6 MB (49.79%) - LeetSync

---
+++ 4216-weighted-word-mapping/weighted-word-mapping.java
@@ -0,0 +1,24 @@
+class Solution {
+    public String mapWordWeights(String[] words, int[] weights) {
+        
+        StringBuilder result = new StringBuilder();
+
+        for (String word : words) {
+            int sum = 0;
+
+            for (char ch : word.toCharArray()) {
+                sum += weights[ch - 'a'];
+            }
+
+            int mod = sum % 26;
+
+            // Reverse mapping:
+            // 0 -> z, 1 -> y, ..., 25 -> a
+            char mappedChar = (char) ('z' - mod);
+
+            result.append(mappedChar);
+        }
+
+        return result.toString();
+    }
+}
