1500d1a616d57e89b52a8c762ed35ed582c4e67a
manasi-navale2107 <navalemanasi@gmail.com>
2026-06-14 09:44:41 +0530

    Time: 0 ms (100.00%) | Memory: 47 MB (15.83%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.java
+++ 141-linked-list-cycle/linked-list-cycle.java
@@ -1,28 +1,23 @@
-/**
- * Definition for singly-linked list.
- * class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode(int x) {
- *         val = x;
- *         next = null;
- *     }
- * }
- */
 public class Solution {
     public boolean hasCycle(ListNode head) {
-        ListNode fast = head;
+
+        if(head == null){
+            return false;
+        }
+
         ListNode slow = head;
+        ListNode fast = head;
 
-        while(fast !=null && fast.next != null){
-            fast = fast.next.next;
+        while(fast != null && fast.next != null){
+
             slow = slow.next;
+            fast = fast.next.next;
 
-            if(fast == slow){
+            if(slow == fast){
                 return true;
             }
         }
+
         return false;
-        
     }
 }
