ea329dbb4d1f1867ce41bb6c565886a8584df0e2
manasi-navale2107 <navalemanasi@gmail.com>
2026-07-01 17:46:47 +0530

    Time: 142 ms (55.56%) | Memory: 124.3 MB (27.62%) - LeetSync

---
+++ 2914-find-the-safest-path-in-a-grid/find-the-safest-path-in-a-grid.java
@@ -0,0 +1,73 @@
+class Solution {
+    public int maximumSafenessFactor(List<List<Integer>> grid) {
+        int n = grid.size();
+
+        int[][] dist = new int[n][n];
+        for (int[] row : dist) {
+            Arrays.fill(row, -1);
+        }
+
+        Queue<int[]> q = new LinkedList<>();
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid.get(i).get(j) == 1) {
+                    dist[i][j] = 0;
+                    q.add(new int[]{i, j});
+                }
+            }
+        }
+
+        int[][] dirs = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+
+        while (!q.isEmpty()) {
+            int[] curr = q.poll();
+            int r = curr[0];
+            int c = curr[1];
+
+            for (int[] d : dirs) {
+                int nr = r + d[0];
+                int nc = c + d[1];
+
+                if (nr >= 0 && nr < n && nc >= 0 && nc < n && dist[nr][nc] == -1) {
+                    dist[nr][nc] = dist[r][c] + 1;
+                    q.add(new int[]{nr, nc});
+                }
+            }
+        }
+
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> b[0] - a[0]);
+        boolean[][] visited = new boolean[n][n];
+
+        pq.add(new int[]{dist[0][0], 0, 0});
+        visited[0][0] = true;
+
+        while (!pq.isEmpty()) {
+            int[] curr = pq.poll();
+
+            int safe = curr[0];
+            int r = curr[1];
+            int c = curr[2];
+
+            if (r == n - 1 && c == n - 1) {
+                return safe;
+            }
+
+            for (int[] d : dirs) {
+                int nr = r + d[0];
+                int nc = c + d[1];
+
+                if (nr >= 0 && nr < n && nc >= 0 && nc < n && !visited[nr][nc]) {
+                    visited[nr][nc] = true;
+                    pq.add(new int[]{
+                        Math.min(safe, dist[nr][nc]),
+                        nr,
+                        nc
+                    });
+                }
+            }
+        }
+
+        return 0;
+    }
+}
