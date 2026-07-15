dbc11563e9fbdbd1d2e70d948fa405bd214be77b
manasi-navale2107 <navalemanasi@gmail.com>
2026-07-15 17:54:39 +0530

    Time: 34 ms (46.77%) | Memory: 94.7 MB (81.73%) - LeetSync

---
+++ 1966-frequency-of-the-most-frequent-element/frequency-of-the-most-frequent-element.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int maxFrequency(int[] nums, int k) {
+
+    Arrays.sort(nums);
+    int left = 0;
+    long windowSum = 0;
+    int maxFreq = 1;
+
+    for(int right = 0; right < nums.length; right++){
+        windowSum += nums[right];
+
+        while((long)nums[right]*(right - left + 1) - windowSum >k){
+            windowSum -= nums[left];
+            left++;
+        }
+        maxFreq = Math.max(maxFreq, (right - left + 1));
+    }
+
+    return maxFreq;
+        
+    }
+}
