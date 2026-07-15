17d5a8b4734a5ee1524af9c1f93aff1b6581eb4b
manasi-navale2107 <navalemanasi@gmail.com>
2026-07-15 14:44:59 +0530

    Time: 0 ms (100.00%) | Memory: 44.7 MB (32.82%) - LeetSync

--- 31-next-permutation/next-permutation.java
+++ 31-next-permutation/next-permutation.java
@@ -1,32 +1,45 @@
 class Solution {
+
     public void nextPermutation(int[] nums) {
+
         int i = nums.length - 2;
 
-        while(i>=0 && nums[i+1]<= nums[i]){
+        // Step 1: Find pivot
+        while (i >= 0 && nums[i] >= nums[i + 1]) {
             i--;
         }
-        if(i>=0){
-            int j = nums.length -1;
-            while(nums[j] <= nums[i]){
+
+        // Step 2: Find next greater element
+        if (i >= 0) {
+
+            int j = nums.length - 1;
+
+            while (nums[j] <= nums[i]) {
                 j--;
             }
-            swap(nums,i,j);
+
+            swap(nums, i, j);
         }
-        reverse(nums, i+1);  
+
+        // Step 3: Reverse suffix
+        reverse(nums, i + 1, nums.length - 1);
     }
 
-    public void swap(int[] nums, int i, int j){
+    public void swap(int[] nums, int i, int j) {
+
         int temp = nums[i];
         nums[i] = nums[j];
         nums[j] = temp;
     }
 
-    public void reverse(int[] nums, int i){
-        int j = nums.length - 1;
-        while(i<j){
-            swap(nums,i,j);
-            i++;
-            j--;
+    public void reverse(int[] nums, int left, int right) {
+
+        while (left < right) {
+
+            swap(nums, left, right);
+
+            left++;
+            right--;
         }
     }
 }
