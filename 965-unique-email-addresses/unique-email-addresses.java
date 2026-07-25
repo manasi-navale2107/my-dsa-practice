61e1a4d69fe13f2c5aee61572e79101dd8663ae3
manasi-navale2107 <navalemanasi@gmail.com>
2026-07-25 17:24:13 +0530

    Time: 13 ms (47.97%) | Memory: 46.7 MB (47.76%) - LeetSync

---
+++ 965-unique-email-addresses/unique-email-addresses.java
@@ -0,0 +1,29 @@
+class Solution {
+
+    public int numUniqueEmails(String[] emails) {
+
+        HashSet<String> set = new HashSet<>();
+
+        for (String email : emails) {
+
+            String[] parts = email.split("@");
+
+            String local = parts[0];
+            String domain = parts[1];
+
+            int plusIndex = local.indexOf('+');
+
+            if (plusIndex != -1) {
+                local = local.substring(0, plusIndex);
+            }
+
+            local = local.replace(".", "");
+
+            String normalized = local + "@" + domain;
+
+            set.add(normalized);
+        }
+
+        return set.size();
+    }
+}
